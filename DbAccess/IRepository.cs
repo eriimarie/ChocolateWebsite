9cf7e354bae5bd704581a92c9f4d20b68219e637
eriimarie <74576575+eriimarie@users.noreply.github.com>
2022-06-15 20:11:26 -0400

    Add files via upload

---
+++ DbAccess/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using ChocoShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ChocoShop.DbAccess
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        TEntity GetByID(object id);
+        void Insert(TEntity entity);
+        void Update(TEntity entityToUpdate);
+    }
+}
