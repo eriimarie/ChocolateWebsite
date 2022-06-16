1586a79f20ed4a2341d3a19f17a5aa9ce57f6bba
eriimarie <74576575+eriimarie@users.noreply.github.com>
2022-06-15 20:11:40 -0400

    Add files via upload

---
+++ Extensions/ImageConstants.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChocoShop.Extensions
+{
+    public static class ImageConstants
+    {
+        public static string thumbnail = "thumb";
+        public static string detail = "detail";
+
+        public static string GetThumbnaill(int Id, IWebHostEnvironment _environment)
+        {
+
+            string wwwRootPath = _environment.WebRootPath;
+            string path = Path.Combine(wwwRootPath + "/product/", Id.ToString());
+            if (System.IO.Directory.Exists(path))
+            {
+                var files = System.IO.Directory.GetFiles(path);
+
+                foreach (var file in files)
+                {
+                    var fileInfo = new FileInfo(file);
+                    var displayPath = "/product/" + Id + "/" + fileInfo.Name;
+                    if (file.Contains(ImageConstants.thumbnail))
+                    {
+                        return displayPath;
+                    }
+
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,50 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ChocoShop.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToHash(this string unHashed)
+        {
+            using (var x = new HMACMD5 { Key = Encoding.ASCII.GetBytes("PKey") })
+            {
+                var data = Encoding.ASCII.GetBytes(unHashed);
+                data = x.ComputeHash(data);
+                return Encoding.ASCII.GetString(data);
+            }
+        }
+
+        public static void SendEmail(string subject, string to, string from, string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(from));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+            // send email
+            var username = "noemail@gmail.com";
+            var password = "No_Password";
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(username, password);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+
+        public static MvcHtmlString If(this MvcHtmlString value, bool evaluation)
+        {
+            return evaluation ? value : MvcHtmlString.Empty;
+        }
+    }
+}
